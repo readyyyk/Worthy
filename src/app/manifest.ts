67e09eee8bd1619f324994094f1b44ed5cdede8d
readyyyk <mr.sairexer@gmail.com>
2024-02-03 21:44:09 +0300

    [F] Manifest, logos

---
+++ src/app/manifest.ts
@@ -0,0 +1,37 @@
+import { type MetadataRoute } from 'next';
+
+export default function manifest(): MetadataRoute.Manifest {
+    return {
+        name: 'Worthy',
+        short_name: 'Worthy',
+        theme_color: '#6D28D9',
+        background_color: '#030712',
+        start_url: '/',
+        display: 'standalone',
+        orientation: 'portrait',
+        icons: [
+            {
+                'src': '/logo-192.png',
+                'sizes': '192x192',
+                'type': 'image/png',
+                'purpose': 'any',
+            },
+            {
+                'src': '/logo-192.png',
+                'sizes': '192x192',
+                'type': 'image/png',
+                'purpose': 'maskable',
+            },
+            {
+                'src': '/logo-384.png',
+                'sizes': '384x384',
+                'type': 'image/png',
+            },
+            {
+                'src': '/logo-512.png',
+                'sizes': '512x512',
+                'type': 'image/png',
+            },
+        ],
+    };
+}
